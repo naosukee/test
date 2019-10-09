fdec0ecd9905f3152eccb479c3fc24e98c8133b8
naosukee <businessnao126@gmail.com>
2019-10-09 19:49:14 +0900

    10月9日

--- WebProj3/src/com/internousdev/webproj3/action/WelcomeAction.java
+++ WebProj3/src/com/internousdev/webproj3/action/WelcomeAction.java
@@ -2,18 +2,10 @@ package com.internousdev.webproj3.action;
 
 import com.opensymphony.xwork2.ActionSupport;
 
-<<<<<<< HEAD
 public class WelcomeAction extends ActionSupport{
 	public String execute(){
 		return SUCCESS;
 	}
 
 }
-=======
 
-public class WelcomeAction extends ActionSupport{
- public String execute() {
-        return SUCCESS;
-  }
-}
->>>>>>> 641accf1859d0ce9f4d092aa63acb4d5eda781a9
