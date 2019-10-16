81ec1859347f6839b64ba39c049aaf23aa13fb1b
naosukee <businessnao126@gmail.com>
2019-10-16 19:44:49 +0900

    10月16日

--- WebProj5/src/com/internousdev/webproj5/action/HelloStrutsAction.java
+++ WebProj5/src/com/internousdev/webproj5/action/HelloStrutsAction.java
@@ -2,23 +2,27 @@ package com.internousdev.webproj5.action;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
 
 import com.internousdev.webproj5.dao.HelloStrutsDAO;
 import com.internousdev.webproj5.dto.HelloStrutsDTO;
 import com.opensymphony.xwork2.ActionSupport;
 
-public class HelloStrutsAction extends ActionSupport {
+public class HelloStrutsAction extends ActionSupport implements SessionAware{
 
- private List<HelloStrutsDTO> HelloStrutsDTOList = new ArrayList<HelloStrutsDTO>();
-
+ private List<HelloStrutsDTO> helloStrutsDTOList = new ArrayList<HelloStrutsDTO>();
+ private Map<String,Object> session;
  public String execute() {
   String ret=ERROR;
   HelloStrutsDAO dao=new HelloStrutsDAO();
 
-  HelloStrutsDTOList=dao.select();
+  helloStrutsDTOList=dao.select();
 
 
-  if(HelloStrutsDTOList.size() > 0){
+  if(helloStrutsDTOList.size() > 0){
+	  session.put("helloStrutsDTOList", helloStrutsDTOList);
    ret=SUCCESS;
   }else{
    ret=ERROR;
@@ -29,10 +33,19 @@ public class HelloStrutsAction extends ActionSupport {
 
 
  public List<HelloStrutsDTO> getHelloStrutsDTOList() {
-  return HelloStrutsDTOList;
+  return helloStrutsDTOList;
  }
 
  public void setHelloStrutsDTOList(List<HelloStrutsDTO> helloStrutsDTOList) {
-  HelloStrutsDTOList = helloStrutsDTOList;
+  this.helloStrutsDTOList = helloStrutsDTOList;
+ }
+
+ public Map<String, Object> getSession() {
+	 return session;
+ }
+
+ @Override
+ public void setSession(Map<String, Object> session) {
+	 this.session =session;
  }
 }
